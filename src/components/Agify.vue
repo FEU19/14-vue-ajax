758dfa0d38cfaa96e6d70db5e18ed7a465228080
David Andersson <lejonmanen@gmail.com>
2020-03-31 15:20:46 +0200

    User friendly slow API

--- src/components/Agify.vue
+++ src/components/Agify.vue
@@ -1,17 +1,42 @@
 <template>
-	<section>
-		<h2>Agify API demo</h2>
-		<p>
-			<input type="text" v-model="name" />
-			<button @click="getData">Get data</button> <br />
-			<button @click="getDataAsync">Get data async</button>
-			<button @click="getDataAxios">Get data (axios)</button>
-			<button @click="getDataAxiosAsync">Get data (axios, async)</button>
-		</p>
-		<p v-if="model">
-			Out of {{model.count}} people, the average age of {{model.name}} is {{model.age}}.
-		</p>
-	</section>
+	<div>
+		<section>
+			<h2>Agify API demo</h2>
+			<p>
+				<input type="text" v-model="name" />
+				<button @click="getData">Get data</button> <br />
+				<button @click="getDataAsync">Get data async</button>
+				<button @click="getDataAxios">Get data (axios)</button>
+				<button @click="getDataAxiosAsync">Get data (axios, async)</button>
+			</p>
+			<p v-if="model">
+				Out of {{model.count}} people, the average age of {{model.name}} is {{model.age}}.
+			</p>
+		</section>
+
+		<hr />
+
+		<section>
+			<h2>"Slow" API demo</h2>
+			<p>
+				We simulate a slow API by waiting a short while before sending the real request.
+			</p>
+			<p>
+				Delay: <input type="number" v-model="delay" />
+				<button @click="sendSlowRequest"
+					:disabled="waitingForResponse">Send request</button>
+			</p>
+			<p v-if="errorOccurred">
+				An error occurred, please try again.
+			</p>
+			<p v-else-if="model2">
+				{{model2.count}} people.
+			</p>
+			<p v-else>
+				No data from API.
+			</p>
+		</section>
+	</div>
 </template>
 
 <script>
@@ -20,7 +45,11 @@ const baseUrl = 'https://api.agify.io';
 export default {
 	data: () => ({
 		name: '',
-		model: null //{ name, age, count }
+		model: null, //{ name, age, count }
+		model2: null,
+		delay: 2000,
+		waitingForResponse: false,
+		errorOccurred: false
 	}),
 	methods: {
 		// Ett problem med fetch är att det inte finns ett inbyggt sätt att skicka querystring parametrar. Man måste säkra dem manuellt. Därför använder Angular och Vue andra bibliotek. Tyvärr är det så som fetch-standarden är definierad. Exempel på kod som löser problemet finns här: https://github.com/github/fetch/issues/256
@@ -84,11 +113,44 @@ export default {
 			} catch(error) {
 				console.log('Something went wrong', error);
 			}
+		},
+
+		sendSlowRequest() {
+			this.waitingForResponse = true;
+			this.errorOccurred = false;
+			setTimeout(this.sendRequestForReal, this.delay);
+		},
+		async sendRequestForReal() {
+			// const url = 'https://cat-fact.herokuapp.com/facts/random';
+			// let response = await this.$http.get(url, {
+			// 	params: { animal_type: 'cat', amount: '2' }
+			// });
+			// console.log('Cat facts:', response.data);
+			try {
+				let response = await this.$http.get(baseUrl, {
+					params: { name: this.name }
+				});
+				console.log('sendRequestForReal', response.data);
+				this.model2 = {
+					name:  response.data.name,
+					age:   response.data.age,
+					count: response.data.count
+				}
+			} catch(error) {
+				console.log('Something went wrong', error);
+				this.model2 = null;
+				this.errorOccurred = true;
+			} finally {
+				this.waitingForResponse = false;
+			}
 		}
 	}
 }
 </script>
 
 <style scoped>
-
+button:disabled {
+	color: lightgray;
+	background-color: white;
+}
 </style>
