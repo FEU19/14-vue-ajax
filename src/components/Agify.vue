e30d20e8ff68c10a6a5f003e8dd40d259ff8b3d5
David Andersson <lejonmanen@gmail.com>
2020-03-31 12:03:35 +0200

    Explanation about fetch and querystring

--- src/components/Agify.vue
+++ src/components/Agify.vue
@@ -21,6 +21,7 @@ export default {
 		model: null //{ name, age, count }
 	}),
 	methods: {
+		// Ett problem med fetch är att det inte finns ett inbyggt sätt att skicka querystring parametrar. Man måste säkra dem manuellt. Därför använder Angular och Vue andra bibliotek. Tyvärr är det så som fetch-standarden är definierad. Exempel på kod som löser problemet finns här: https://github.com/github/fetch/issues/256
 		getData() {
 			fetch(baseUrl + `?name=${this.name}` // VARNING! osäker kod, lita inte på användaren
 			)
